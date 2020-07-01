df5370ab68e1daee334a0262a2d6107b2ab7427d
Paul Nykiel <paul.nykiel@gmail.com>
2020-07-02 00:40:20 +0200

    Doc for instruction_util.hpp

--- instruction_util.hpp
+++ instruction_util.hpp
@@ -2,21 +2,36 @@
  * @file instruction_util.hpp
  * @author paul
  * @date 04.06.20
- * Description here TODO
+ * Definition of utility structs for the implementation of instructions.
  */
 #ifndef TEMPLATE_CPU_INSTRUCTION_UTIL_HPP
 #define TEMPLATE_CPU_INSTRUCTION_UTIL_HPP
 
+/**
+ * Get the next program counter used for conditional jumps.
+ * @tparam cond if the bool is false PC+1 is returned, else target is returned
+ * @tparam PC the program counter for the if case
+ * @tparam target the program counter for the else case
+ */
 template<bool cond, std::size_t PC, std::size_t target>
 struct GetTarget {
     static constexpr std::size_t val = PC+1;
 };
 
+/**
+ * Specialization for GetTarget if cond==true
+ */
 template<std::size_t PC, std::size_t target>
 struct GetTarget<true, PC, target> {
     static constexpr std::size_t val = target;
 };
 
+/**
+ * Struct for implementing a division with exception if the result is undefined
+ * @tparam T the type of the operands
+ * @tparam a the numerator
+ * @tparam b the denominator
+ */
 template<typename T, T a, T b>
 struct DivWithException {
     static_assert(b != 0, "Division by zero!");
