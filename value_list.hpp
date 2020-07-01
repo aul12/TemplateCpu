652e49d372968e715f68e59b92188fc078040087
Paul Nykiel <paul.nykiel@gmail.com>
2020-07-01 23:45:34 +0200

    Value List Doc

--- value_list.hpp
+++ value_list.hpp
@@ -2,19 +2,30 @@
  * @file value_list.hpp
  * @author paul
  * @date 25.05.20
- * Description here TODO
+ * Header related to the value list and all related structs and concepts.
  */
 #ifndef TEMPLATE_CPU_VALUE_LIST_HPP
 #define TEMPLATE_CPU_VALUE_LIST_HPP
 
 #include "type_list.hpp"
 
+/**
+ * Container used to wrap a value into a type.
+ * @tparam T the type of the value
+ * @tparam val_ the value
+ */
 template<typename T, T val_>
 struct ValueContainer {
     using type = T;
     static constexpr auto val = val_;
 };
 
+// Concepts
+
+/**
+ * Struct to check if a type is a value_container
+ * @tparam T the type ti check
+ */
 template<typename T>
 struct IsValueContainer {
     static constexpr auto val = false;
@@ -25,9 +36,17 @@ struct IsValueContainer<ValueContainer<T, val_>> {
     static constexpr auto val = true;
 };
 
+/**
+ * Concept for value_container, i.e. check if a type is an instantiation of ValueContainer
+ * @tparam T the type to check
+ */
 template<typename T>
 concept value_container = IsValueContainer<T>::val;
 
+/**
+ * Check if a type is a value list, that is a type list with elements which are value containers.
+ * @tparam T the type to check
+ */
 template<type_list T>
 struct IsValueList {
     static constexpr auto val = IsValueContainer<typename T::elem>::val && IsValueList<typename T::next>::val;
@@ -38,9 +57,18 @@ struct IsValueList<ListEnd> {
     static constexpr auto val = true;
 };
 
+/**
+ * Concept for the definition of a value list based on IsValueList
+ * @tparam T the type to check
+ */
 template<typename T>
 concept value_list = IsValueList<T>::val;
 
+/**
+ * Check if a value list is of a certain type (i.e. all types in the value containers are of the certain type).
+ * @tparam T the type to check
+ * @tparam List the list to check
+ */
 template<typename T, value_list List>
 struct ListOfType {
     static constexpr auto val =
@@ -52,26 +80,57 @@ struct ListOfType<T, ListEnd> {
     static constexpr auto val = true;
 };
 
+/**
+ * Concept to check if a value list of type.
+ * @tparam T the type
+ * @tparam List the list
+ */
 template<typename T, typename List>
 concept value_list_of_type = ListOfType<T, List>::val;
 
 
 // Helper functions
+
+/**
+ * Get the value at a certain index in a value_list
+ * @tparam List the list
+ * @tparam index the index needs to be less than the size of the list
+ */
 template<value_list List, std::size_t index>
 struct GetVal {
     static constexpr auto val = GetType<List, index>::type::val;
 };
 
+/**
+ * Set a value inside a value list. This will return a new list with the changed value.
+ * @tparam T the type of the new value
+ * @tparam List the list on which to work
+ * @tparam index the index at which to change
+ * @tparam val the new value
+ */
 template<typename T, value_list List, std::size_t index, T val> requires value_list_of_type<T, List>
 struct SetVal {
     using type = typename SetType<ValueContainer<T, val>, List, index>::type;
 };
 
+/**
+ * Check if value list contains a value. Be careful, the types need to be identical as well. For example
+ * 0 of type int is not equal to 0 of type short, long, char, unsigned, float, double...
+ * @tparam T the type of the value
+ * @tparam val_ the value to find
+ * @tparam list the list
+ */
 template<typename T, T val_, value_list list>
 struct ContainsVal {
     static constexpr auto val = ContainsType<ValueContainer<T, val_>, list>::val;
 };
 
+/**
+ * Create a value list of a single type of a variadic list of values.
+ * @tparam T the type to use
+ * @tparam t the first value
+ * @tparam ts the other values
+ */
 template<typename T, T t, T... ts>
 struct FromVariadicVal {
     using type = Type<ValueContainer<T, t>, typename FromVariadicVal<T, ts...>::type>;
@@ -82,6 +141,12 @@ struct FromVariadicVal<T, t> {
     using type = Type<ValueContainer<T, t>, ListEnd>;
 };
 
+/**
+ * Fill a value list with N values of a fixed type
+ * @tparam N the number of elements
+ * @tparam T the type of the elements
+ * @tparam t the value of the elements
+ */
 template<std::size_t N, typename T, T t>
 struct FillVal {
     using type = typename FillType<N, ValueContainer<T, t>>::type;
