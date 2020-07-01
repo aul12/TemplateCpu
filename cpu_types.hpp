0e0f0371ef8929f2c1cce316e18f8481495ccc0c
Paul Nykiel <paul.nykiel@gmail.com>
2020-07-02 00:32:12 +0200

    cpu_types.hpp doc

--- cpu_types.hpp
+++ cpu_types.hpp
@@ -2,18 +2,25 @@
  * @file cpu_types.hpp
  * @author paul
  * @date 25.05.20
- * Description here TODO
+ * Types used for the cpu
  */
 #ifndef TEMPLATE_CPU_CPU_TYPES_HPP
 #define TEMPLATE_CPU_CPU_TYPES_HPP
 
 #include "config.hpp"
 
+/**
+ * Registers are a value list of base_type type
+ * @tparam T the type to check
+ */
 template<typename T>
 concept Registers = IsValueList<T>::val && ListOfType<base_type, T>::val;
 
+/**
+ * Memory is a value list of base_type type
+ * @tparam T the type to check
+ */
 template<typename T>
 concept Memory = IsValueList<T>::val && ListOfType<base_type, T>::val;
-
 
 #endif //TEMPLATE_CPU_CPU_TYPES_HPP
